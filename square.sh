e4df7e0458f647cc67f6d3a83ca3a227c3ac0e24
Jonathan Mohrbacher <johnnymo87@gmail.com>
2022-07-09 04:09:17 -0400

    Require target path, handle spaces in path

--- square.sh
+++ square.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+IFS=$'\n'
 set -euo pipefail
+
+# Make sure we've been called with an indir
+# https://stackoverflow.com/a/3945952
+if [ $# -ne 1 ]; then
+  echo "Must be called with the path to the images!"
+  exit 2
+fi
+indir=$1
 
 outdir=squares
 mkdir -p $outdir
 
-for image in $(find . -name '*.jpg')
+for image in $(find $indir -name '*.jpg')
 do
   echo "Squaring $image..."
   # https://legacy.imagemagick.org/Usage/thumbnails/#square
